b9a9367a5c8a483b7bd6290c531b55a04f76df0e
Douglas Berkley <douglasmberkley@gmail.com>
2020-02-20 20:27:55 +0900

    added spinner on load

--- app/javascript/components/submit.js
+++ app/javascript/components/submit.js
@@ -2,6 +2,8 @@ const submitPhoto = () => {
   const file = document.getElementById('upload_photo')
   if (file) {
     file.addEventListener('change', (event) => {
+      const label = document.querySelector('label')
+      label.innerHTML = "<i class='fas fa-compact-disc fa-spin'></i>"
       const form = document.getElementById('new_upload')
       form.submit()
     })
