66239f897ee1b4b277da29bbb5cb4e28977faa85
Douglas Berkley <douglasmberkley@gmail.com>
2020-02-20 15:38:54 +0900

    cleaned up js auto submit

--- app/javascript/components/submit.js
+++ app/javascript/components/submit.js
@@ -1,13 +1,9 @@
-import Rails from '@rails/ujs'
-
 const submitPhoto = () => {
   const file = document.getElementById('upload_photo')
   if (file) {
-    console.log('found file');
     file.addEventListener('change', (event) => {
       const form = document.getElementById('new_upload')
-      console.log('change happened and found form');
-      Rails.fire(form,'submit')
+      form.submit()
     })
   }
 }
