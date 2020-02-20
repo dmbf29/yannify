34ff8308df1b13bdbb54ef043a6be8a0eb815ae7
Douglas Berkley <douglasmberkley@gmail.com>
2020-02-20 15:21:27 +0900

    Added style to new page

---
+++ app/javascript/components/submit.js
@@ -0,0 +1,12 @@
+import Rails from '@rails/ujs'
+
+const submitPhoto = () = {
+  const file = document.getElementById('upload_photo')
+  if (file) {
+    file.addEventListener('change', (event) => {
+
+    })
+  }
+}
+
+export { submitPhoto }
